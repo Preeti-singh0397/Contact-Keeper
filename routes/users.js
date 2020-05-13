0e61c2dcc431477eab6090e7a73a6af6517e351b
Preeti-singh0397 <63150571+Preeti-singh0397@users.noreply.github.com>
2020-05-13 19:27:08 +0530

    hashing password and register route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,48 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/User");
 
 // @router POST api/users
 //@desc Register user
 // @access public
-router.post('/',(req,res)=>{
-res.send("Register User");
-})
+router.post(
+  "/",
+  [
+    check("name", "Please enter the name").not().notEmpty(),
+    check("email", "Please enter the valid email").isEmail(),
+    check("password", "Please enter the valid password").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
 
-module.exports=router;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ mes: "User already exists" });
+      }
+      user = new User()({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      res.send("User Saved");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express=require("express");
+const connectDB=require("./config/db");
 
 const app=express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({extended:false}))
 
 app.get('/',(req,res)=>res.json({mes:'Welcome to contact keeper'}));
 
@@ -8,6 +15,7 @@ app.get('/',(req,res)=>res.json({mes:'Welcome to contact keeper'}));
 app.use('/api/users',require('./routes/users'));
 app.use('/api/contact',require('./routes/contact'));
 app.use('/api/auth',require('./routes/auth'));
+
 
 
 const PORT=process.env.PORT||5000;
